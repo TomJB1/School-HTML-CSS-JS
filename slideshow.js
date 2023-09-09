d2a63e3f82b770e4cf88052737e12a8ae25a413d
Tom <tombrandisicloud@gmail.com>
2023-09-09 21:12:03 +0100

    simplifying js

--- slideshow.js
+++ slideshow.js
@@ -2,18 +2,13 @@ image = document.getElementById("slideshow");
 nextButton = document.getElementById("nextPic");
 lastButton = document.getElementById("lastPic");
 
-nextButton.addEventListener("click", NextPicture);
-lastButton.addEventListener("click", LastPicture);
+nextButton.addEventListener("click", () => ManualChange(1));
+lastButton.addEventListener("click", () => ManualChange(-1));
 
 let pictureNames = ["pictures/scenery1.jpg", "pictures/scenery2.webp", "pictures/scenery3.jpg", "pictures/scenery4.jpg"]
 let imageNum = 0;
 let autoNext = true;
-let length = pictureNames.length
-
-function NextPicture(){ ManualChange(1); }
-
-function LastPicture(){ ManualChange(-1); }
-
+let length = pictureNames.length;
 
 function ManualChange(amount)
 {
