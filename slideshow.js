79fef689f10851dbd0fc512e0ebaa177fae72030
Tom <tombrandisicloud@gmail.com>
2023-09-09 21:03:24 +0100

    simplify image number wrapping using mod

--- slideshow.js
+++ slideshow.js
@@ -8,6 +8,7 @@ lastButton.addEventListener("click", LastPicture);
 let pictureNames = ["pictures/scenery1.jpg", "pictures/scenery2.webp", "pictures/scenery3.jpg", "pictures/scenery4.jpg"]
 let imageNum = 0;
 let autoNext = true;
+let length = pictureNames.length
 
 function NextPicture(){ ManualChange(1); }
 
@@ -32,14 +33,7 @@ function AutoNext()
 function ChangePic(amount)
 {
     imageNum += amount;
-    if(imageNum > pictureNames.length -1)
-    {
-        imageNum = 0;
-    }
-    else if(imageNum < 0)
-    {
-        imageNum = pictureNames.length -1;
-    }
+    imageNum = (imageNum%length + length)%length;
     image.src = pictureNames[imageNum];
 }
 
