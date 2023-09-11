10d871441f07ae619cb351a252e261965aa47bf7
Tom <tombrandisicloud@gmail.com>
2023-09-11 10:06:34 +0100

    add function to minimise all dropdowns

--- contactDropdown.js
+++ contactDropdown.js
@@ -8,9 +8,18 @@ contactDiv = document.getElementById("contact");
 let tArray = Array(buttons.length).fill(0);
 //0 up, 1 down
 
+function minimiseAll()
+{
+    tArray.fill(0);
+    for (let i = 0; i < buttons.length; i++) {
+        dropdown[i].style.height = "0px";
+    }
+}
+
 
 function toggleDropdown(buttonNum)
 {
+    minimiseAll();
     t = tArray[buttonNum];
     t = [1, 0][t];
     tArray[buttonNum] = t;
