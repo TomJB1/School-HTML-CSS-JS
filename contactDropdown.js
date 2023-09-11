d237e4d218a5d0ecca48e209e1316ea68f1b8f49
Tom <tombrandisicloud@gmail.com>
2023-09-11 10:02:53 +0100

    add multiple dropdowns

--- contactDropdown.js
+++ contactDropdown.js
@@ -1,13 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("contactButton").addEventListener("click", toggleDropdown);
-    contactDiv = document.getElementById("contact");
-});
-
-let t = 0;
+buttons = document.querySelectorAll(".dropdownButton");
+dropdown = document.querySelectorAll(".dropdown");
+for (let i = 0; i < buttons.length; i++)
+{
+    buttons[i].addEventListener("click", () => toggleDropdown(i));
+}
+contactDiv = document.getElementById("contact");
+let tArray = Array(buttons.length).fill(0);
 //0 up, 1 down
 
-function toggleDropdown()
+
+function toggleDropdown(buttonNum)
 {
+    t = tArray[buttonNum];
     t = [1, 0][t];
-    contactDiv.style.height = 200*t + "px";
+    tArray[buttonNum] = t;
+    dropdown[buttonNum].style.height = 200*t + "px";
 }
