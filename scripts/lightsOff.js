b9d11892912aaed3819b49964ebf15d4a212b2ca
Tom <tombrandisicloud@gmail.com>
2023-09-14 13:11:56 +0100

    tidy up js

--- scripts/lightsOff.js
+++ scripts/lightsOff.js
@@ -2,9 +2,9 @@ star = document.querySelector("*").style;
 let t = 0
 //0 light, 1 dark
 document.getElementById("body").addEventListener("dblclick", (event) => {
-    textC = ["white", "black"][t];
-    bgC = ["black", "white"][t];
+    t = [1, 0][t];
+    textC = ["black", "white"][t];
+    bgC = ["white", "black"][t];
     star.color = textC;
     star.backgroundColor = bgC;
-    t = [1, 0][t];
 })
