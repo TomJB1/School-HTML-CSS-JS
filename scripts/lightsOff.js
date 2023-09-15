44d288889d72bf9cce675dc241b21149ba06a9e8
Tom <tombrandisicloud@gmail.com>
2023-09-15 12:22:29 +0100

    simplify lights off script

--- scripts/lightsOff.js
+++ scripts/lightsOff.js
@@ -3,8 +3,6 @@ let t = 0
 //0 light, 1 dark
 document.getElementById("body").addEventListener("dblclick", (event) => {
     t = [1, 0][t];
-    textC = ["black", "white"][t];
-    bgC = ["white", "black"][t];
-    star.color = textC;
-    star.backgroundColor = bgC;
+    star.color = ["black", "white"][t];
+    star.backgroundColor = ["white", "black"][t];
 })
